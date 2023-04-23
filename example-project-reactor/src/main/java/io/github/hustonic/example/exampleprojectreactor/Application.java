bd0c939f4b6c980f88ff908b130c389943b533c8
Huston <hustonic@gmail.com>
2023-04-24 02:01:50 +0800

    fix

---
+++ example-project-reactor/src/main/java/io/github/hustonic/example/exampleprojectreactor/Application.java
@@ -0,0 +1,14 @@
+package io.github.hustonic.example.exampleprojectreactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Huston
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
